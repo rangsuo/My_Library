e9e643d97ef3c99e54329bf0271289679babef32
rangsuo <rangsuokoen27@gmail.com>
2020-10-12 16:50:52 +0530

    add Upbutton in the BookActivity and clicking on it works the same as back button Pressed.

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.example.mylibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -32,6 +34,18 @@ public class AllBooksActivity extends AppCompatActivity {
         adapter.setBooks(Utils.getInstance().getAllBooks());
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
